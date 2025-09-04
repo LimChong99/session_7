de3b3a731d385771f866ecf5b6502e73c9682e79
LimChong99 <j279994@tafe.wa.edu.au>
2025-09-04 13:39:19 +0800

    Update any_old_code.py

--- any_old_code.py
+++ any_old_code.py
@@ -1,7 +1,7 @@
 list_of_fruits=["longan", \
                 "rambutan",\
                     "John Viogt is ml", \
-                        "McAfee", "Mark", "Andrew Bolt"]
+                        "McAfee", "Mark", "Andrew Bolt", "Billy Jeams", "Ipoh"]
 for fruit_of_burden in list_of_fruits:
     print(fruit_of_burden)  
 list_of_fruits.append('rambutanlongan')
