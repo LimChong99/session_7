924491ae7f84085ec347219c08e6798d692e3ccd
YeeLim Chong <Limchong900@gmail.com>
2025-09-04 14:04:57 +0800

    hello

--- any_old_code.py
+++ any_old_code.py
@@ -1,7 +1,9 @@
 list_of_fruits=["longan", \
                 "rambutan",\
                     "John Viogt is ml", \
-                        "McAfee", "Mark", "Andrew Bolt", "Billy Jeams", "Ipoh", "Pipipi"]
+                        "mcAfee", "Mark", "Andrew Bolt", 
+                        "billy Jeams", 
+                        "ipoh", "Come on MAN"]
 for fruit_of_burden in list_of_fruits:
     print(fruit_of_burden)  
 list_of_fruits.append('rambutanlongan')
